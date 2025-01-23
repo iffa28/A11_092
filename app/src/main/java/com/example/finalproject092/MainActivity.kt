e3e6228ad6068daa8088958df8cec9a2907f870e
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-23 20:39:51 +0700

    memanggil perpustakaan app untuk memulai aplikasi

--- app/src/main/java/com/example/finalproject092/MainActivity.kt
+++ app/src/main/java/com/example/finalproject092/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finalproject092.ui.theme.FinalProject092Theme
+import com.example.finalproject092.ui.viewModel.PerpustakaanApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinalProject092Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    PerpustakaanApp(
+                        modifier = Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FinalProject092Theme {
-        Greeting("Android")
     }
 }
