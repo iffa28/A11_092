b1b8653a6e2cc6e0912353308e6553b57409095d
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-23 18:12:34 +0700

    menambahkan alamat alamat navigasi untuk entitas buku

--- app/src/main/java/com/example/finalproject092/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/finalproject092/ui/navigation/AlamatNavigasi.kt
@@ -7,3 +7,24 @@ interface AlamatNavigasi {
 object HomeDestination : AlamatNavigasi {
     override val route = "home"
 }
+
+//Buku
+object BookDestination : AlamatNavigasi {
+    override val route = "book"
+}
+
+object InsertBookDestination : AlamatNavigasi {
+    override val route = "insert_book"
+}
+
+object DetailBookDestination : AlamatNavigasi {
+    override val route = "detail_book"
+    const val idBook = "idBuku"
+    val routeWithArgs = "$route/{$idBook}"
+}
+object UpdateBookDestination : AlamatNavigasi {
+    override val route = "update_book"
+    const val idBook = "idBuku"
+    val routeWithArgs = "$route/{$idBook}"
+}
+
