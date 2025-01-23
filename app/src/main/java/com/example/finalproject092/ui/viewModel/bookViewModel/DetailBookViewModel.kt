d6e96260db403e5cca44423676d74de2da25684d
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-23 20:30:05 +0700

    menambahkan view model untuk mengelola logika operasi untuk halaman Detail Buku

---
+++ app/src/main/java/com/example/finalproject092/ui/viewModel/bookViewModel/DetailBookViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.finalproject092.ui.viewModel.bookViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject092.model.Buku
+import com.example.finalproject092.repository.BukuRepository
+import com.example.finalproject092.ui.navigation.DetailBookDestination
+import kotlinx.coroutines.launch
+
+sealed class DetailBookUiState {
+    data class Success(val buku: Buku) : DetailBookUiState()
+    object Error : DetailBookUiState()
+    object Loading : DetailBookUiState()
+}
+
+// ViewModel untuk mengelola data dan status UI pada DetailMhs
+class DetailBookViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val bukuRepo: BukuRepository
+) : ViewModel() {
+
+    // Mendapatkan NIM dari `SavedStateHandle`. Jika tidak ada, akan memunculkan exception.
+    private val idBuku: Int = checkNotNull(savedStateHandle[DetailBookDestination.idBook])
+    var detailBookUiState: DetailBookUiState by mutableStateOf(DetailBookUiState.Loading)
+        private set
+
+    init {
+        getBookbyId()
+    }
+
+    // Fungsi untuk mengambil data mahasiswa berdasarkan NIM
+    fun getBookbyId() {
+        viewModelScope.launch {
+            detailBookUiState = DetailBookUiState.Loading
+            detailBookUiState = try {
+                DetailBookUiState.Success(bukuRepo.getBukubyId(idBuku))
+            } catch (e: Exception) {
+                DetailBookUiState.Error
+            }
+        }
+
+    }
+
+}
