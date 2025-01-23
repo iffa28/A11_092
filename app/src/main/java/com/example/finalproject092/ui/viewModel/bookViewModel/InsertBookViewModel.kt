fa2a0eca9281c2772c90129c507524b9708eb1f3
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-23 20:29:30 +0700

    menambahkan view model untuk mengelola logika operasi untuk halaman insert Buku

---
+++ app/src/main/java/com/example/finalproject092/ui/viewModel/bookViewModel/InsertBookViewModel.kt
@@ -0,0 +1,64 @@
+package com.example.finalproject092.ui.viewModel.bookViewModel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject092.model.Buku
+import com.example.finalproject092.repository.BukuRepository
+import kotlinx.coroutines.launch
+
+class InsertBookViewModel(
+    private val bukuRepo : BukuRepository
+): ViewModel() {
+    var uiState by mutableStateOf(InsertBookUiState())
+        private set
+
+    fun updateInsertBukuState(insertBookUiEvent: InsertBookUiEvent){
+        uiState = InsertBookUiState(insertBookUiEvent = insertBookUiEvent)
+    }
+
+    suspend fun insertBook(){
+        viewModelScope.launch {
+            try {
+                bukuRepo.insertBuku(uiState.insertBookUiEvent.toBk())
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+
+}
+
+fun InsertBookUiEvent.toBk(): Buku = Buku(
+    idBuku = 0,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
+fun Buku.toUiStateBook(): InsertBookUiState = InsertBookUiState(
+    insertBookUiEvent = toInsertBookUiEvent()
+)
+
+fun Buku.toInsertBookUiEvent(): InsertBookUiEvent = InsertBookUiEvent(
+    idBuku = idBuku,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
+data class InsertBookUiState(
+    val insertBookUiEvent: InsertBookUiEvent = InsertBookUiEvent()
+)
+
+data class InsertBookUiEvent(
+    val idBuku: Int = 0,
+    val judul: String = "",
+    val penulis: String = "",
+    val kategori: String = "",
+    val status: String = ""
+)
