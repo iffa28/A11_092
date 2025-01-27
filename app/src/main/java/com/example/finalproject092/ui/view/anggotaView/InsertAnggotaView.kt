18c8f7fb04b177f9897fdf8c1b71b9c7f47c9729
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-28 02:21:46 +0700

    mengubah tampilan insert anggota

--- app/src/main/java/com/example/finalproject092/ui/view/anggotaView/InsertAnggotaView.kt
+++ app/src/main/java/com/example/finalproject092/ui/view/anggotaView/InsertAnggotaView.kt
@@ -1,12 +1,17 @@
 package com.example.finalproject092.ui.view.anggotaView
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -16,9 +21,12 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalproject092.R
 import com.example.finalproject092.ui.topAppBar.CustomTopBar
 import com.example.finalproject092.ui.viewModel.anggotaViewModel.InsertAnggotaViewModel
 import com.example.finalproject092.ui.viewModel.anggotaViewModel.InsertMembersUiEvent
@@ -70,37 +78,52 @@ fun EntryBody(
     onSaveClick: () -> Unit,
     modifier: Modifier = Modifier
 ){
-    Column(
-        verticalArrangement = Arrangement.spacedBy(18.dp),
-        modifier = modifier.padding(12.dp)
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        colorResource(id = R.color.main),
+                        colorResource(id = R.color.contain)
+                    )
+                )
+            )
+            .padding(start=8.dp, end=8.dp)
     ) {
-        FormInput(
-            insertMembersUiEvent = insertMembersUiState.insertMembersUiEvent,
-            onValueChange = onSiswaValueChange,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Button(
-            onClick = onSaveClick,
-            shape = MaterialTheme.shapes.small,
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            verticalArrangement = Arrangement.spacedBy(18.dp),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(10.dp)
+                .background(
+                    color = colorResource(id = R.color.contain),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .padding(16.dp)
         ) {
-            Text(text = "Simpan")
+            FormInput(
+                insertMembersUiEvent = insertMembersUiState.insertMembersUiEvent,
+                onValueChange = onSiswaValueChange,
+                onSaveClick = onSaveClick,
+                modifier = Modifier.fillMaxWidth()
+            )
         }
-
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInput(
     insertMembersUiEvent: InsertMembersUiEvent,
     modifier: Modifier = Modifier,
     onValueChange: (InsertMembersUiEvent) -> Unit = {},
+    onSaveClick: () -> Unit,
     enabled: Boolean = true
 ) {
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
+        modifier = modifier.padding(5.dp),
+        verticalArrangement = Arrangement.spacedBy(30.dp)
     ) {
         OutlinedTextField(
             value = insertMembersUiEvent.nama,
@@ -108,7 +131,7 @@ fun FormInput(
             label = { Text(text = "Nama") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
-            singleLine = true
+            singleLine = true,
         )
         OutlinedTextField(
             value = insertMembersUiEvent.idAnggota,
@@ -135,5 +158,20 @@ fun FormInput(
             enabled = enabled,
             singleLine = true
         )
+
+        Button(
+            onClick = onSaveClick,
+            shape = RoundedCornerShape(10.dp),
+            modifier = Modifier.fillMaxWidth(),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.HomeTil),
+                contentColor = colorResource(id = R.color.white)
+            ),
+            elevation = ButtonDefaults.elevatedButtonElevation(
+                defaultElevation = 8.dp
+            )
+        ) {
+            Text(text = "Simpan", style = MaterialTheme.typography.bodyLarge)
+        }
     }
 }
