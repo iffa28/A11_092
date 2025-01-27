0aac4b29dab40c9556990328a67c031550328e34
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-28 02:34:30 +0700

    mendaftarkan view model CRUD Pengembalian, dan viewmodel CRUD peminjaman

--- app/src/main/java/com/example/finalproject092/ui/viewModel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/finalproject092/ui/viewModel/PenyediaViewModel.kt
@@ -14,6 +14,14 @@ import com.example.finalproject092.ui.viewModel.bookViewModel.DetailBookViewMode
 import com.example.finalproject092.ui.viewModel.bookViewModel.HomeBookViewModel
 import com.example.finalproject092.ui.viewModel.bookViewModel.InsertBookViewModel
 import com.example.finalproject092.ui.viewModel.bookViewModel.UpdateBookViewModel
+import com.example.finalproject092.ui.viewModel.peminjamanViewModel.DetailPeminjamanViewModel
+import com.example.finalproject092.ui.viewModel.peminjamanViewModel.HomePeminjamanViewModel
+import com.example.finalproject092.ui.viewModel.peminjamanViewModel.InsertPeminjamanViewModel
+import com.example.finalproject092.ui.viewModel.peminjamanViewModel.UpdatePeminjamanViewModel
+import com.example.finalproject092.ui.viewModel.pengembalianViewModel.DetailPengembalianViewModel
+import com.example.finalproject092.ui.viewModel.pengembalianViewModel.HomePengembalianViewModel
+import com.example.finalproject092.ui.viewModel.pengembalianViewModel.InsertPengembalianViewModel
+import com.example.finalproject092.ui.viewModel.pengembalianViewModel.UpdatePengembalianViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -66,6 +74,66 @@ object PenyediaViewModel {
             UpdateAnggotaViewModel(
                 createSavedStateHandle(),
                 appBook().container.anggotaRepository
+            )
+        }
+
+        //peminjaman
+        initializer {
+            HomePeminjamanViewModel(
+                appBook().container.peminjamanRepository
+            )
+        }
+
+        initializer {
+            InsertPeminjamanViewModel(
+                appBook().container.peminjamanRepository,
+                appBook().container.bukuRepository,
+                appBook().container.anggotaRepository
+
+            )
+        }
+
+        initializer {
+            DetailPeminjamanViewModel(
+                createSavedStateHandle(),
+                appBook().container.peminjamanRepository
+            )
+        }
+
+        initializer {
+            UpdatePeminjamanViewModel(
+                createSavedStateHandle(),
+                appBook().container.peminjamanRepository,
+                appBook().container.bukuRepository,
+                appBook().container.anggotaRepository
+            )
+        }
+
+        //pengembalian
+        initializer {
+            HomePengembalianViewModel(
+                appBook().container.pengembalianRepository
+            )
+        }
+        initializer {
+            InsertPengembalianViewModel(
+                appBook().container.pengembalianRepository,
+                appBook().container.peminjamanRepository
+            )
+        }
+
+        initializer {
+            DetailPengembalianViewModel(
+                createSavedStateHandle(),
+                appBook().container.pengembalianRepository
+            )
+        }
+
+        initializer {
+            UpdatePengembalianViewModel(
+                createSavedStateHandle(),
+                appBook().container.pengembalianRepository,
+                appBook().container.peminjamanRepository
             )
         }
     }
