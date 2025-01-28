5f210381c0780d82229e4b6a4e6d78db8fdfd901
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-28 13:56:34 +0700

    menambahkan fungsi untuk mengatur tampilan update pengembalian

---
+++ app/src/main/java/com/example/finalproject092/ui/view/pengembalianView/UpdatePengembalianView.kt
@@ -0,0 +1,54 @@
+package com.example.finalproject092.ui.view.pengembalianView
+
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finalproject092.ui.topAppBar.CustomTopBar
+import com.example.finalproject092.ui.viewModel.PenyediaViewModel
+import com.example.finalproject092.ui.viewModel.pengembalianViewModel.UpdatePengembalianViewModel
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdatePengembalianScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    updateViewModel: UpdatePengembalianViewModel = viewModel(factory = PenyediaViewModel.Factory),
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(updateViewModel.updatePgUiState.insertPgUiEvent.idPeminjaman) {
+        updateViewModel.updateInsertPgState(updateViewModel.updatePgUiState.insertPgUiEvent)
+        Log.d("UpdatePengembalianScreen", "Memuat data pengembalian untuk idPeminjaman: ${updateViewModel.updatePgUiState.insertPgUiEvent.idPeminjaman}")
+    }
+    Scaffold(
+        topBar = {
+            CustomTopBar(
+                title = "Edit Pengembalian",
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryBody(
+            insertPgUiState = updateViewModel.updatePgUiState,
+            onPgValueChange = updateViewModel::updateInsertPgState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    updateViewModel.updatePengembalian()
+                    onNavigateUp()
+                }
+            },
+            peminjamanList = updateViewModel.peminjamanList,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
