31fea5a2d7efe01203e1299675726fdbb7a19123
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2025-01-28 15:54:31 +0700

    menambahkan launch effect untuk memanggil judul dan nama

--- app/src/main/java/com/example/finalproject092/ui/view/peminjamanView/UpdatePeminjamanView.kt
+++ app/src/main/java/com/example/finalproject092/ui/view/peminjamanView/UpdatePeminjamanView.kt
@@ -1,9 +1,11 @@
 package com.example.finalproject092.ui.view.peminjamanView
 
+import android.util.Log
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,9 +22,18 @@ fun UpdatePeminjamanScreen(
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
     updateViewModel: UpdatePeminjamanViewModel = viewModel(factory = PenyediaViewModel.Factory),
-    viewmodel: InsertPeminjamanViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
     val coroutineScope = rememberCoroutineScope()
+    LaunchedEffect(updateViewModel.updatePjUiState.insertPjUiEvent.idAnggota) {
+        updateViewModel.updateInsertPjState(updateViewModel.updatePjUiState.insertPjUiEvent)
+        Log.d("UpdatePeminjamanScreen", "Memuat data pengembalian untuk IdAnggota: ${updateViewModel.updatePjUiState.insertPjUiEvent.idAnggota}")
+    }
+
+    LaunchedEffect(updateViewModel.updatePjUiState.insertPjUiEvent.idBuku) {
+        updateViewModel.updateInsertPjState(updateViewModel.updatePjUiState.insertPjUiEvent)
+        Log.d("UpdatePeminjamanScreen", "Memuat data pengembalian untuk IdAnggota: ${updateViewModel.updatePjUiState.insertPjUiEvent.idBuku}")
+    }
+
     Scaffold(
         topBar = {
             CustomTopBar(
@@ -42,8 +53,8 @@ fun UpdatePeminjamanScreen(
                     onNavigateUp()
                 }
             },
-            bukuList = viewmodel.bukuList,
-            anggotaList = viewmodel.anggotaList,
+            bukuList = updateViewModel.bukuList,
+            anggotaList = updateViewModel.anggotaList,
             modifier = Modifier.padding(innerPadding),
 
         )
